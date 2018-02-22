c79eaa9364a2a04f99573ab5317c52d8156e8787
Ophus Wong <ophus.wong@gmail.com>
2018-02-22 12:51:57 -0700

    Lint App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,10 +1,11 @@
+/* eslint-disable */
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
 describe('APP', () => {
   it('should match the snapshot', () => {
-    const renderedComponent = shallow (<App />);
+    const renderedComponent = shallow(<App />);
 
     expect(renderedComponent).toMatchSnapshot();
   });
