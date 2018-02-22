962c831414a7780e910d78f53afc7195ffa0354e
Ophus Wong <ophus.wong@gmail.com>
2018-02-22 11:00:16 -0700

    Add tests for helper and mockData

--- src/helper.test.js
+++ src/helper.test.js
@@ -1,9 +1,17 @@
 import helper from './helper';
+import { mockIntialFetch } from './mockData';
 
 describe('HELPER', () => {
   describe('fetchAndParse', () => {
     it('should call fetch with the correct params', () => {
-
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+        json: () => Promise.resolve({
+          mockIntialFetch
+        })
+      }))
     });
+    const url = 'http://localhost:3001/pokemon/'
+
+    expect(fetchAndParse(url)).toEqual(mockIntialFetch)
   });
 });
---
+++ src/mockData.js
@@ -0,0 +1,11 @@
+export const mockInitialFetch = [
+  {id: "1", name: "normal", pokemon: Array(5)},
+  {id: "2", name: "fighting", pokemon: Array(5)},
+  {id: "3", name: "flying", pokemon: Array(5)},
+  {id: "4", name: "poison", pokemon: Array(5)},
+  {id: "5", name: "ground", pokemon: Array(5)},
+  {id: "6", name: "rock", pokemon: Array(5)},
+  {id: "7", name: "bug", pokemon: Array(5)},
+  {id: "8", name: "ghost", pokemon: Array(5)},
+  {id: "9", name: "steel", pokemon: Array(5)}
+];
